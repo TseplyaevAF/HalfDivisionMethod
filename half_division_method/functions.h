37d8d94a402856d8424a1e46d7c8431ce762be9d
Alex <tseplyaeff.alex@yandex.ru>
2019-12-05 13:13:26 +0900

    fixes

--- half_division_method/functions.h
+++ half_division_method/functions.h
@@ -3,3 +3,7 @@
 
 double f1(double x);
 double f2(double x);
+double f1_1(double x);
+double f2_1(double x);
+double f1_2(double x);
+double f2_2(double x);
--- half_division_method/half_division_method.cpp
+++ half_division_method/half_division_method.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-double num_method(double a, double b, double c, unsigned n) {
-	double e = 0.0001, x = 0;
+double num_method(double a, double b, unsigned n, unsigned &m) {
+	double e = 0.0001, x = 0, c = 0;
 	
 	if (n == 1) {
 
@@ -14,6 +14,7 @@ double num_method(double a, double b, double c, unsigned n) {
 				b = c; else
 				if (f1(b) * f1(c) < 0)
 					a = c;
+			m++;
 		}
 		return x = (a + b) / 2;
 	}
@@ -25,6 +26,7 @@ double num_method(double a, double b, double c, unsigned n) {
 				b = c; else
 				if (f2(b) * f2(c) < 0)
 					a = c;
+			m++;
 		}
 		return x = (a + b) / 2;
 	}
@@ -34,12 +36,20 @@ double num_method(double a, double b, double c, unsigned n) {
 int main()
 {
 	setlocale(0, "RUS");
-	double a = -1, b = 0, // отрезок неопределённости
-	c = 0, x = 0; unsigned n = 2;
 
-	x = num_method(a, b, c, n);
+	// Отрезки неопределенности:
+	// 1) a = -3, b = -1
+	// 2) a = -1, b = 0
+	//	  a = 0, b = 1
+	//    a = 1, b = 1.5
+
+	double a = 0, b = 1, // отрезок неопределённости
+	c = 0, x = 0; unsigned m = 0; // m - кол-во итераций в цикле
+
+	x = num_method(a, b, 2, m);
 
 	cout << "x = " << x << "\n";
+	cout << "Количество итераций: " << m << "\n";
 
 	system("pause");
 
