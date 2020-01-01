e373d7229b7c7687f5a09f761bdd04c03f992ed8
Alex <tseplyaeff.alex@yandex.ru>
2020-01-01 13:44:39 +0900

    .

--- half_division_method/functions.cpp
+++ half_division_method/functions.cpp
@@ -9,23 +9,3 @@ double f1(double x) {
 double f2(double x) {
 	return 4 * pow(x, 3) - 5 * pow(x, 2) - 2 * x + 2;
 }
-
-// производная первого порядка функции под а
-double f1_1(double x) {
-	return pow(5, x) * log(5) + 3;
-}
-
-// производная второго порядка функции под а
-double f1_2(double x) {
-	return pow(5, x) * pow(log(5), 2);
-}
-
-// производная первого порядка функции под б
-double f2_1(double x) {
-	return 12 * pow(x, 2) - 10 * x - 2;
-}
-
-// производная второго порядка функции под б
-double f2_2(double x) {
-	return 24 * x - 10;
-}
